a2419f9c6b4c1252bb4fbfcd2c0787fddba0ee2c
dinbandhu12 <dinbandhu8445@gmail.com>
2024-08-03 20:35:43 +0530

    adding user mention features and fixing the commit button

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import Loader from "@/components/Loader";
-import { getClerkUsers } from "@/lib/actions/user.actions";
+import { getClerkUsers, getDocumentUsers } from "@/lib/actions/user.actions";
+import { useUser } from "@clerk/nextjs";
 import {
   ClientSideSuspense,
   LiveblocksProvider,
@@ -9,6 +10,9 @@ import {
 import { ReactNode } from "react";
 
 const Provider = ({ children }: { children: React.ReactNode }) => {
+
+  const { user: clerkUser } = useUser();
+
   return (
     <LiveblocksProvider 
       authEndpoint="/api/liveblocks-auth"
@@ -17,6 +21,14 @@ const Provider = ({ children }: { children: React.ReactNode }) => {
         return users;
       }}
     
+      resolveMentionSuggestions={async ({ text, roomId}) => {
+        const roomUsers = await getDocumentUsers({ 
+          roomId,
+          currentUser: clerkUser?.emailAddresses[0].emailAddress!,
+          text,
+        });
+        return roomUsers;
+      }}
     >
       <ClientSideSuspense fallback={<Loader />}>{children}</ClientSideSuspense>
     </LiveblocksProvider>
